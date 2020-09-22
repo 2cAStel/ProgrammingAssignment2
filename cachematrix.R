df8acc16581a41b3203439dbb2f2aa51db0a1483
Arthur de Castel <arthurdecastel@live.fr>
2020-09-22 20:05:58 +0200

    1st commit for my submission ofmy programming assignement 2 : Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to create a special object 
+## that stores a numeric vector and cache's its mean.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing 4 functions to : 
+## - set the value of the matrix in the cache
+## - get the value of the matrix
+## - set the value of the inverse in the cache
+## - get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inverse) inv <<- inverse
+	getinv <- function() inv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and 
+## sets the value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinv()
+	if (!is.null(inv)) {
+		message ("getting cached data")
+		return(m) 
+	}
+	data <- x$get()
+	inv <- solve(data)
+	x$setinv(inv)
+	inv
 }
